fd6178bf81d1e44af69d74f2232879bfedea0830
Andrzej Malinowski <andmalin6@gmail.com>
2019-04-13 13:21:34 +0200

    initial commit

--- projektOX/Program.cs
+++ projektOX/Program.cs
@@ -7,6 +7,24 @@ namespace projektOX
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            string imieGraczaA = "";
+            string imieGraczaB = "";
+
+            char znakGraczaA = 'O';
+            char znakGraczaB = 'X';
+
+            char[,] plansza = new char[3, 3]
+            {
+                {'1', '2', '3' },
+                {'4', '5', '6' },
+                {'7', '8', '9' }
+            };
+
+            Console.Write("Wpisz imie gracza A: ");
+            imieGraczaA = Console.ReadLine();
+            Console.Write("Wpisz imie gracza B: ");
+            imieGraczaB = Console.ReadLine();
         }
     }
 }
